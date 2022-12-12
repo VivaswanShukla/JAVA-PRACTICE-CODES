dc0f3078330f86dd9e71253dd88042c68bfe001f
Vivaswan Shukla <107896809+VivaswanShukla@users.noreply.github.com>
2022-12-12 22:11:03 +0530

    Add files via upload

---
+++ autoboxing.java
@@ -0,0 +1,10 @@
+package com.codes;
+
+public class autoboxing {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer i = Integer.valueOf(a);
+        int j = a;
+        System.out.println(a+ " " + i + " " + j);
+    }
+}
